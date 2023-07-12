4ef386da7a91adeb8b1be9ebbd3935d602a90982
kifa888 <kifa878@gmail.com>
2023-07-12 14:12:20 +0300

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
+import playerGuidance from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name} !`);
+playerGuidance();
