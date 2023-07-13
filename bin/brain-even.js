c308e75cc1082cbf0c6635b5a906953b0f5c600f
kifa888 <kifa878@gmail.com>
2023-07-13 19:23:55 +0300

    update bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
-import {greetings} from '../src/games/even.js';
-
+import greetings from '../src/games/even.js';
 
 console.log('Welcome to the Brain Games!');
 greetings();
-/*console.log('Answer "yes" if the number is even, otherwise answer "no".');
-randomNumber();
-answers();*/
