f7d9fe149b92cc6ba08ed7e0822c0fd0592d3b4f
kifa888 <113115248+kifa888@users.noreply.github.com>
2023-07-21 17:00:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import readlineSync from 'readline-sync';
 import getRandomNum from './games/getRandomNumber.js';
 
-export const greetings = () => console.log('Welcome to the Brain Games!';
+export const greetings = () => console.log('Welcome to the Brain Games!');
 export const userName = () => readlineSync.question('May I have your name? ') 
 export const greetingsUser = () => console.log(`Hello, ${userName}!`)
 
