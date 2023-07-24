bd69c59f4b61cc5a2fd4bc6a3923c7a63a3bc716
kifa888 <kifa878@gmail.com>
2023-07-24 18:14:36 +0300

    update bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,15 +1,4 @@
 #!/usr/bin/env node
-<<<<<<< HEAD
 import greetings from '../src/games/even.js';
 
 greetings();
-=======
-
-import { greetings, userName, greetingsUser, gameConditionsEven, calculationEven }from '../src/index.js';
-
-greetings();
-userName();
-greetingsUser();
-gameConditionsEven();
-calculationEven();
->>>>>>> c61541bfe20896f86bc488bf8abf959e118de8d2
