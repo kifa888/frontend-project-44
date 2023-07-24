bb6259b9caecde1144fedf77ebe6caee4d51bd47
kifa888 <kifa878@gmail.com>
2023-07-24 18:15:05 +0300

    update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,30 +1,26 @@
 import readlineSync from 'readline-sync';
 import getRandomNumber from './getRandomNumber.js';
+import userName from '../index.js';
 
-
+const rounds = 3;
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const greetings = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let score = 0;
-  while (score < 3) {
-    const randomNumber = getRandomNumber(1, 99);
+  for (let i = 0; i < rounds; i += 1) {
+    const randomNumber = getRandomNumber(1, 30);
     console.log(`Question: ${randomNumber}`);
     const userResponse = readlineSync.question('Your answer: ');
     if ((userResponse === 'yes' && randomNumber % 2 === 0) || (userResponse === 'no' && randomNumber % 2 !== 0)) {
       console.log('Correct!');
-      score += 1;
     } else if (userResponse === 'yes' && randomNumber % 2 !== 0) {
-      return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${userName}!`);
+      return console.log(`'no' is wrong answer ;(. Correct answer was '${userResponse}'.\nLet's try again, ${userName}!`);
     } else if (userResponse === 'no' && randomNumber % 2 === 0) {
-      return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${userName}!`);
-    } else return console.log(`Let's try again, ${userName}!`);
-    if (score === 3) {
-      return console.log(`Congratulations, ${userName}!`);
-    }
+      return console.log(`'no' is wrong answer ;(. Correct answer was '${userResponse}'.\nLet's try again, ${userName}!`);
+    } else return console.log(`'${userResponse}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${userName}!`);
   }
+
+ console.log(`Congratulations, ${userName}!`);
+  
 };
 
 export default greetings;
