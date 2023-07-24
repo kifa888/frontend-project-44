ce780a472c66da0274774e70722e531eaeda5ff0
kifa888 <kifa878@gmail.com>
2023-07-24 18:14:53 +0300

    update src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,6 +1,37 @@
-import startGameBrain from '../index.js';
 import getRandomNumber from '../games/getRandomNumber.js';
+import readlineSync from 'readline-sync';
+import userName from '../index.js';
 
-const conditionText = 'What is the result of the expression?';
+const rounds = 3;
+console.log('What is the result of the expression?');
+
+const calculator = () => {
+  for (let i = 0; i < rounds; i += 1) {
+  const operations = ['+', '-', '*'];
+  const getRandomOperations = operations[Math.floor(Math.random() * operations.length)];
+  const generator1 = getRandomNumber(1, 10);
+  const generator2 = getRandomNumber(1, 10);
+  const equation = `${generator1} ${getRandomOperations} ${generator2}`;
+  console.log(`Question: ${equation}`);
+  const userResponse = readlineSync.question('Your answer: ')
+  let getCalculationSigns;
+  if (getRandomOperations === '+') {
+    getCalculationSigns = `${generator1 + generator2}`
+    } else if (getRandomOperations === '-') {
+    getCalculationSigns = `${generator1 - generator2}`
+    } else if (getRandomOperations === '*') {
+    getCalculationSigns = `${generator1 * generator2}`
+    }
+
+  if (userResponse === getCalculationSigns) {
+    console.log('Correct!');
+    } else {
+    console.log(`${userResponse} is wrong answer ;(. Correct answer was ${getCalculationSigns}.\nLet's try again, ${userName}`)
+    return;
+    }
+  }
+  console.log (`Congratulations, ${userName}!`);
+}
 
 
+export default calculator;
