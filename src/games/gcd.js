d1c9a39a174a104c178f2e68705a80d070f09243
kifa888 <kifa878@gmail.com>
2023-07-24 20:26:46 +0300

    update src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from './getRandomNumber.js';
+import userName from '../index.js';
+import getGCD from './nod.js';
+
+
+const rounds = 3;
+console.log('Find the greatest common divisor of given numbers.');
+
+const commonDivisors = () => {
+  for (let i = 0; i < rounds; i += 1) {
+  const number1  = getRandomNumber(1, 100);
+  const number2 = getRandomNumber(1, 100);
+  console.log (`Question: ${number1} ${number2}`);
+  const result = getGCD(number1, number2)
+  const userResponse = readlineSync.question('Your answer: ');
+  if (userResponse === String(result)) {
+    console.log('Correct!');
+   } else {
+     return console.log(`${userResponse} is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`)
+   }
+  }
+  console.log(`Congratulations, ${userName}!`);
+}
+
+export default commonDivisors;
