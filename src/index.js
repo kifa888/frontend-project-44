3f29ed977b06d5c2353326e2504028ef53f7b767
kifa888 <kifa878@gmail.com>
2023-07-26 15:22:19 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
+const rounds = 3;
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export default userName;
+export {userName, rounds, getRandomNumber };
