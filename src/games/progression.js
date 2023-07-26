ed2ae8c09af3cfffa42eaaf20e0a7a9a54500507
kifa888 <kifa878@gmail.com>
2023-07-26 15:24:02 +0300

    update src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-import getRandomNumber from './getRandomNumber.js';
 import { userName, rounds } from '../index.js';
 
 console.log('What number is missing in the progression?');
