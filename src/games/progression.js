072f3523aca2c681b033f6b8eff81ee7213201d4
kifa888 <kifa878@gmail.com>
2023-08-01 14:43:05 +0300

    update  src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,16 +1,39 @@
 import readlineSync from 'readline-sync';
-import { userName, rounds } from '../index.js';
+import { userName, rounds, getRandomNumber } from '../index.js';
 
 console.log('What number is missing in the progression?');
 
+
 const determiningNumber = () => {
-    for (let i = 0; i < rounds; i+=1) {
-        
-        
-        
+    for (let i = 0; i < rounds; i += 1) {
+       const firstNumber = getRandomNumber(1, 30);
+       const step = getRandomNumber(1, 10);
+       const progressionlength = getRandomNumber(6, 8);
+       const array = [];
+       let result;
+       for (let j = 1; j < progressionlength; j += 1) {
+        result = firstNumber + (step * j)
+        array.push(result);
+       }
+       const getArrayIndex = array[Math.floor(Math.random() * array.length)]
+       const numberToString = getArrayIndex.toString();
+       
+       const indexToСolon = array.indexOf(getArrayIndex);
+       array[indexToСolon] = '..';
+       const str = array.join(' ');
+    
 
+       console.log(`Question: ${str}`)
+       const userResponse = readlineSync.question('Your answer: ');
 
-
-    }
-
+       if (userResponse === numberToString) {
+        console.log('Correct!');
+       } else return console.log(`'${userResponse}' is wrong answer ;(. Correct answer was '${getArrayIndex}'.\nLet's try again, ${userName}!`)
+    } 
+    console.log(`Congratulations, ${userName}!`);
+    
+    
 }
+
+
+export default determiningNumber; 
