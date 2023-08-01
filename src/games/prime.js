b24427027684d841ff2314319e9ac14a5559fe78
kifa888 <kifa878@gmail.com>
2023-08-01 18:18:34 +0300

    update src/games/prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import { userName, rounds, getRandomNumber } from '../index.js';
+import isPrime from '../games/prime-function.js'
+
+console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`)
+
+const primeGame= () => {
+    for(let i = 0; i < rounds; i += 1) {
+       const randomNumber = getRandomNumber(1, 17);
+       const primeNumber = isPrime(randomNumber);
+       
+       console.log(`Question: ${randomNumber}`)
+
+       const userResponse = readlineSync.question('Your answer: ');
+
+       
+      let correctAnswer;
+      if (primeNumber === true) {
+          correctAnswer = 'yes'
+      } else if (primeNumber === false) {
+        correctAnswer = 'no'} 
+    
+      if (userResponse === correctAnswer) {
+          console.log('Correct!');
+      } else return console.log(`'${userResponse}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`)
+    }
+    console.log(`Congratulations, ${userName}!`);
+
+}
+
+
+
+
+export default primeGame;
