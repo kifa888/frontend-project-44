7a4e1f18f249946e7fdfb3518258d2c8c63d53e6
kifa888 <kifa878@gmail.com>
2023-08-01 19:32:43 +0300

    update src/games/nod-gcd.js

--- src/games/nod-gcd.js
+++ src/games/nod-gcd.js
@@ -1,16 +1,18 @@
-const getGCD = (a, b) => {
-    let c;
-    if (a > b) {
-      while (b !== 0) {
-        c = b;
-        b = a % b;
-        a = c;
-      }
+/* eslint-disable no-param-reassign */
+const getGCD = (num1, num2) => {
+  let result;
+  if (num1 > num2) {
+    while (num2 !== 0) {
+      result = num2;
+      num2 = num1 % num2;
+      num1 = result;
     }
-    if (a < b) {
-     return getGCD(b, a);
-    }
-    return c;
-   }
+  }
+  if (num1 < num2) {
+    return getGCD(num2, num1);
+  }
+  return result;
+};
+/* eslint-enable no-param-reassign */
 
-   export default getGCD;
+export default getGCD;
