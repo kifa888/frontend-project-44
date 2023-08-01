18fa3814cc6fad91ee7f0a3bbc109e4c344155c2
kifa888 <kifa878@gmail.com>
2023-08-01 19:31:54 +0300

    update src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,20 +4,21 @@ import getGCD from './nod-gcd.js';
 
 console.log('Find the greatest common divisor of given numbers.');
 
-const commonDivisors = () => {
+const commonDivisors = (number) => {
   for (let i = 0; i < rounds; i += 1) {
-  const number1  = getRandomNumber(1, 100);
-  const number2 = getRandomNumber(1, 100);
-  console.log (`Question: ${number1} ${number2}`);
-  const result = getGCD(number1, number2)
-  const userResponse = readlineSync.question('Your answer: ');
-  if (userResponse === String(result)) {
-    console.log('Correct!');
-   } else {
-     return console.log(`${userResponse} is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`)
-   }
+    const number1 = getRandomNumber(1, 100);
+    const number2 = getRandomNumber(1, 100);
+    console.log(`Question: ${number1} ${number2}`);
+    const result = getGCD(number1, number2);
+    const userResponse = readlineSync.question('Your answer: ');
+    if (userResponse === String(result)) {
+      console.log('Correct!');
+    } else {
+      return console.log(`${userResponse} is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`);
+    }
   }
   console.log(`Congratulations, ${userName}!`);
-}
+  return number;
+};
 
 export default commonDivisors;
