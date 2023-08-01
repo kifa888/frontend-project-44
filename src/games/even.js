d1a7e0cea08136e525612c9fff04c3b4517e2243
kifa888 <kifa878@gmail.com>
2023-08-01 19:30:50 +0300

    update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,12 +3,12 @@ import { userName, rounds, getRandomNumber } from '../index.js';
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const greetings = () => {
+const greetings = (number) => {
   for (let i = 0; i < rounds; i += 1) {
     const randomNumber = getRandomNumber(1, 30);
     console.log(`Question: ${randomNumber}`);
     const userResponse = readlineSync.question('Your answer: ');
-    const correctAnswer =  randomNumber % 2 === 0 ? 'yes': 'no';
+    const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
     if ((userResponse === 'yes' && randomNumber % 2 === 0) || (userResponse === 'no' && randomNumber % 2 !== 0)) {
       console.log('Correct!');
     } else if (userResponse === 'yes' && randomNumber % 2 !== 0) {
@@ -18,8 +18,8 @@ const greetings = () => {
     } else return console.log(`'${userResponse}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
   }
 
- console.log(`Congratulations, ${userName}!`);
-  
+  console.log(`Congratulations, ${userName}!`);
+  return number;
 };
 
 export default greetings;
